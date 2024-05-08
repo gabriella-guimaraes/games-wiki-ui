6b32ae59ac5fdf8cc4203bf17c36dec5b4bb4113
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-08 12:05:37 -0300

    feat: main pages routes created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { AddReviewComponent } from './components/add-review/add-review.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'gallery', component: GalleryComponent },
+    { path: 'addReview', component: AddReviewComponent }
+];
