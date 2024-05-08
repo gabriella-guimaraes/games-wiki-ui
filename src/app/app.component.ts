6d16cc7b7e75054430170012ec5a7032a2c9a9b3
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-08 12:25:06 -0300

    feat: navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'games-wiki-ui';
+  title = 'games wiki ui';
 }
+
+//TODO:
+// 1- Editar a NavBar - ok
+// 2 - Criar as rotas no arquivo de rotas - ok
+// 3 - Estilizar componentes
+// 4 - conectar a API (local)
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, Router, RouterLink } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { GalleryComponent } from '../gallery/gallery.component';
+import { AddReviewComponent } from '../add-review/add-review.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, HomeComponent, AboutComponent, GalleryComponent, AddReviewComponent, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  btnMenu: boolean = false;
+
+  btnMobile() {
+    this.btnMenu =!this.btnMenu;
+  }
+
+  constructor(private router: Router) {}
+
+  ngOnInit(){
+    this.router.navigate(["/"]);
+  }
 
 }
