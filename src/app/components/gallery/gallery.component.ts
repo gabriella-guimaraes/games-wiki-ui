1eccf1f8fd9ff5536d2a62679dda9bbbcca49a3d
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-09 17:01:21 -0300

    feat: Gallery Component design

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatButtonModule, MatIconAnchor } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatButtonModule, MatInputModule, MatIconAnchor, MatIconModule, MatButton, MatDividerModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
