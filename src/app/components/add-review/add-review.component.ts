a45751545a760a64309987fac11e322223a175c6
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-10 23:48:32 -0300

    feat: Add Review component

--- src/app/components/add-review/add-review.component.ts
+++ src/app/components/add-review/add-review.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControlComponent } from '../form-control/form-control.component';
 
 @Component({
   selector: 'app-add-review',
   standalone: true,
-  imports: [],
+  imports: [FormControlComponent],
   templateUrl: './add-review.component.html',
   styleUrl: './add-review.component.css'
 })
