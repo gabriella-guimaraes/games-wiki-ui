fbeaa219db855136effc318ae06e280d301b882d
gabriella-guimaraes <gabigara13@gmail.com>
2024-05-13 21:21:05 -0300

    feat: Edit Review component design

---
+++ src/app/components/edit-review/edit-review.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormControlComponent } from '../form-control/form-control.component';
+
+@Component({
+  selector: 'app-edit-review',
+  standalone: true,
+  imports: [FormControlComponent],
+  templateUrl: './edit-review.component.html',
+  styleUrl: './edit-review.component.css'
+})
+export class EditReviewComponent {
+
+}
