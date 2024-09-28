2695b14e6a84b0208063b28ae8dd90f1c8baaac3
gabriella-guimaraes <gabigara13@gmail.com>
2024-09-28 18:58:32 -0300

    feat: Section Title component grid size props

--- src/Components/atoms/sectionTitle/SectionTitle.tsx
+++ src/Components/atoms/sectionTitle/SectionTitle.tsx
@@ -4,16 +4,18 @@ import styles from "./SectionTitle.module.css";
 import { Typography } from '@mui/material';
 
 interface SectionTitleProps {
-    text: string
+    text: string,
+    textSize?: number,
+    dividerSize?: number
 }
 
-function SectionTitle({ text } : SectionTitleProps) {
+function SectionTitle({ text, textSize = 3, dividerSize = 9 } : SectionTitleProps) {
   return (
     <Grid container alignItems="center" justifyContent="space-evenly">
-            <Grid size={{ xs: 12, md: 3 }}>
+            <Grid size={{ xs: 12, md: textSize }}>
                 <Typography variant="h1" align='left' sx={{ fontStyle: "italic"}} className={styles.title}>{text}</Typography>
             </Grid>
-            <Grid size={{ xs: 12, sm: 12, md: 9 }} className={styles.divider}></Grid>
+            <Grid size={{ xs: 12, sm: 12, md: dividerSize }} className={styles.divider}></Grid>
     </Grid>
   )
 }
