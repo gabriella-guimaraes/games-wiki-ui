3e1341a97414356c7d72bdb462c27f16e71b81b4
gabriella-guimaraes <gabigara13@gmail.com>
2024-09-28 16:51:47 -0300

    feat: About Us view - page

--- src/Components/pages/about/About.tsx
+++ src/Components/pages/about/About.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from "./About.module.css";
 import Project from '../../templates/Project/Project';
+import Team from '../../templates/Team/Team';
 
 function About() {
   return (
     <section className={styles.About}>
       <div className={styles.Project}>
         <Project />
+      </div>
+      <div className={styles.Team}>
+        <Team />
       </div>
     </section>
   )
