b11d08ac6fc6a46e51d1e1cb485cc8e7fb0fa62e
gabriella-guimaraes <gabigara13@gmail.com>
2024-10-05 15:27:09 -0300

    feat: rating component position - ReviewInfo component

--- src/Components/organisms/ReviewInfo/ReviewInfo.tsx
+++ src/Components/organisms/ReviewInfo/ReviewInfo.tsx
@@ -21,7 +21,7 @@ function ReviewInfo({ date, title, platform, description, favCharacters, rating 
   return (
     <section className={styles.ReviewInfo}>
         <Grid container alignItems="center" spacing={3}>
-            <Grid size={12}>
+            <Grid size={6}>
               <Typography variant="subtitle1" gutterBottom>
                 Created at: {new Date(date).toLocaleDateString()}
               </Typography>
@@ -35,34 +35,19 @@ function ReviewInfo({ date, title, platform, description, favCharacters, rating 
                 {title}
               </Typography>
             </Grid>
-            <Grid size={12}>
+            <Grid size={6}>
               <Typography variant="body2" gutterBottom>
                 Platform: {platform}
               </Typography>
             </Grid>
+            <Grid size={6}>
+                  <Rating name="read-only" value={rating} readOnly />
+            </Grid>
             <Grid size={12}>
               <Typography>{description}</Typography>
             </Grid>
-            {/* TODO: Transformar isso em um atom + personalização  */}
             <Grid size={12}>
               <CustomAccordion title="Favorite Characters" content={favCharacters} />
-            </Grid>
-            <Grid size={12}>
-              {/* TODO: Transformar isso em um molecule */}
-              <Grid
-                container
-                alignItems="left"
-                display="flex"
-                flexDirection="column"
-                spacing={1}
-              >
-                <Grid size={12}>
-                  <Typography variant="button">Rating</Typography>
-                </Grid>
-                <Grid size={12}>
-                  <Rating name="read-only" value={rating} readOnly />
-                </Grid>
-              </Grid>
             </Grid>
             {/* TODO: Transformar isso em uma molecule */}
             <Grid size={6}>
